d8908783a13585e7ca64360de3137eef6c13983b
livecloud-labs <livecloud.store@gmail.com>
2024-10-23 08:32:12 +0100

    chore: adjust entity export

---
+++ src/database/entities/index.ts
@@ -0,0 +1,59 @@
+import { Organisation } from './organisations';
+import { Application } from './applications';
+import { Credential } from './credentials';
+import { Event } from './events';
+import { CredentialPermission } from './credential_permissions';
+import { CredentialPattern } from './credential_patterns';
+import { Room } from './rooms';
+import { RoomSession } from './room_sessions';
+import { Connection } from './connections';
+import { AdminUser } from './admin_users';
+import { Session } from './sessions';
+import { DeliveryMetric } from './delivery_metrics';
+import { OrganisationAdminUser } from './organisation_admin_users';
+import { ApplicationAdminUser } from './application_admin_users';
+import { AuthenticationUser } from './authentication_users';
+import { AuthenticationUserVerification } from './authentication_user_verification';
+import { AuthenticationUserIdentity } from './authentication_user_identities';
+import { AuthenticationUserMfaFactor } from './authentication_user_mfa_factors';
+import { AuthenticationUserMfaChallenge } from './authentication_user_mfa_challenges';
+import { AuthenticationUsersApplication } from './authentication_users_applications';
+import { AuthenticationProvider } from './authentication_providers';
+import { ApplicationAuthenticationProvider } from './application_authentication_providers';
+import { ApplicationAuthenticationPreference } from './application_authentication_preferences';
+import { AuthenticationActivityLog } from './authentication_activity_logs';
+import { ApplicationWebhook } from './application_webhooks';
+import { WebhookEvents } from './webhook_events';
+import { ApplicationWebhookLog } from './application_webhook_logs';
+import { ApplicationWebhookEvent } from './application_webhook_events';
+
+export default [
+  Organisation,
+  Application,
+  Credential,
+  Event,
+  CredentialPermission,
+  CredentialPattern,
+  Session,
+  DeliveryMetric,
+  Room,
+  RoomSession,
+  Connection,
+  AdminUser,
+  OrganisationAdminUser,
+  ApplicationAdminUser,
+  AuthenticationUser,
+  AuthenticationUserVerification,
+  AuthenticationUserIdentity,
+  AuthenticationUserMfaFactor,
+  AuthenticationUserMfaChallenge,
+  AuthenticationUsersApplication,
+  AuthenticationProvider,
+  ApplicationAuthenticationProvider,
+  ApplicationAuthenticationPreference,
+  AuthenticationActivityLog,
+  ApplicationWebhook,
+  ApplicationWebhookLog,
+  ApplicationWebhookEvent,
+  WebhookEvents
+];
